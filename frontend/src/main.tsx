033443845a41cfd3af937b83e751a3de87def769
Bohdan Myrinets <bohdan.myrinets@innovatrics.com>
2024-12-27 04:46:00 +0100

    yet another env fix

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,7 @@ import React from "react"; // Add this line
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import dotenv from "dotenv";
 import { Auth0Provider } from "@auth0/auth0-react";
-dotenv.config();
 const domain = "dev-ye6rboqfch0cwuik.us.auth0.com";
 const clientId = "V5rE0wHpT72anvpCAQ8B6IJ5NKvOiGTp";
 
