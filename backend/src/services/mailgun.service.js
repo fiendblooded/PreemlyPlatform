7b3543917ab03cd7f50c32488a75227b81a51f07
t.arhipov <t.arhipov@medpoint24.ru>
2025-01-14 19:02:42 +0400

    test

--- backend/src/services/mailgun.service.js
+++ backend/src/services/mailgun.service.js
@@ -1,22 +1,23 @@
+export default sendEmail;
 import formData from "form-data";
 import Mailgun from "mailgun.js";
 import dotenv from "dotenv";
 dotenv.config();
 
+// Настройки Mailgun
 const mailgun = new Mailgun(formData);
 const key = "b57fced339f8b56bda91042862a71e6a-191fb7b6-1a0e98af";
-const mg = mailgun.client({
-  username: "mudkip",
-  key: key,
-  url: "https://api.eu.mailgun.net",
-});
+const domain = "sandbox962334c8dbd348f8b4a8584175564a7b.mailgun.org";
+const mg = mailgun.client({ username: "api", key: key });
 
+// Функция проверки валидности email адреса
 const validateEmail = (email) => {
   const re = /\S+@\S+\.\S+/;
   return re.test(email);
 };
 
-const sendEmail = async (recipient, subject, htmlContent) => {
+// Функция отправки письма
+const sendEmail = async (recipient, subject, body) => {
   if (Array.isArray(recipient)) {
     for (let email of recipient) {
       if (!validateEmail(email)) {
@@ -28,18 +29,27 @@ const sendEmail = async (recipient, subject, htmlContent) => {
   }
 
   try {
-    const response = await mg.messages.create("preemly.eu", {
-      from: "info@preemly.eu",
+    const response = await mg.messages.create(domain, {
+      from: `postmaster@${domain}`,
       to: recipient,
       subject: subject,
-      html: htmlContent,
+      text: body,
     });
 
     return response;
   } catch (error) {
-    console.error("Error in Create Event:", error);
+    console.error("Error in sending email:", error);
     throw error;
   }
 };
 
-export default sendEmail;
+// // Использование функции
+// sendEmail(
+//   "bar@example.com",
+//   "Hello",
+//   "Testing some Mailgun awesomeness!"
+// ).then((response) => {
+//   console.log(response);
+// }).catch((error) => {
+//   console.error(error);
+// });
