451e318f4b432df2d10497f7d007a0109c1a9657
t.arhipov <t.arhipov@medpoint24.ru>
2025-01-15 18:06:29 +0400

    deploy to heroku

--- backend/src/utils/helpers.js
+++ backend/src/utils/helpers.js
@@ -2,6 +2,7 @@ const extractPublicId = (url) => {
   try {
     const parts = url.split("/");
     const fileName = parts[parts.length - 1];
+
     return fileName.split(".")[0];
   } catch (error) {
     console.error("Error extracting public_id:", error.message);
