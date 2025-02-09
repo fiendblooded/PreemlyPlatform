54888ceab57982b3b6e5f40cc89372848719d301
t.arhipov <t.arhipov@medpoint24.ru>
2025-02-09 21:50:28 +0400

    deploy

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,28 +5,31 @@ import bodyParser from 'body-parser';
 import { connectDB } from './config/db';
 import eventRoutes from './routes/event.routes';
 import guestRoutes from './routes/guest.routes';
-import mailRoutes from './routes/mail.routes';
 import userRoutes from './routes/user.routes';
+import mailRoutes from './routes/mail.routes';
 import guestImportRoutes from './routes/guest-import.routes';
 
 dotenv.config();
 
 const app = express();
-app.use(cors());
 
+app.use(cors());
 app.use(bodyParser.json({ limit: '10mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '10mb' }));
 app.use(express.json());
 
+connectDB();
+
+// Routes
 app.use('/api/events', eventRoutes);
 app.use('/api/guests', guestRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/mail', mailRoutes);
 app.use('/api/guest-import', guestImportRoutes);
 
-const PORT = process.env.PORT || 3002;
+
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => {
-  connectDB();
   console.log(`Server is running on PORT:${PORT}`);
 });
